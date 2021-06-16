e364b2b4e50c2ddd7d7435efc26fae3235a6e5b0
Etwas <73107895+Seif-AW@users.noreply.github.com>
2021-06-16 11:57:05 +0200

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import os
 import json
 import jsbeautifier
 import pprint
+import time
 
 from flask import (
     Flask,
@@ -13,6 +14,18 @@ from flask import (
     session,
     url_for
 )
+
+def write() :
+
+    save_path = "/mnt/d/Reports"
+    filename = time.strftime("%Y%m%d%H%M%S.txt")
+    fullname = os.path.join(save_path, filename)
+    msg = session.get('res').replace( '<br','\n')
+    new = "-"* 50
+    f = open(fullname, "+a")
+    f.write("breaches found :"+msg)
+    f.write(msg)
+    f.close  
 
 class User:
     def __init__(self, id, username, password):
@@ -85,21 +98,37 @@ def email():
     if request.method == "POST":
         try :
             input = request.form["email"]
-            print(input)
-#have ibeenpwnd
-            inp = input
-            arg = "python3 ~/graduation/checkpwnedemails/checkpwnedemails.py -a ~/graduation/checkpwnedemails/key.txt " +' -s'  + inp
-            hbpwd = subprocess.run(arg, stdout=subprocess.PIPE ,text=True ,shell=True)
-            print(hbpwd.stdout)  
+            # print(input)
 
+#have ibeenpwnd_tool_canceled
+            # inp = input
+            # arg = "python3 ~/graduation/checkpwnedemails/checkpwnedemails.py -a ~/graduation/checkpwnedemails/key.txt " +' -s'  + inp
+            # hbpwd = subprocess.run(arg, stdout=subprocess.PIPE ,text=True ,shell=True)
+            # print(hbpwd.stdout)  
+
+#haveibeenpwnd
+
+            arg_hbpd = "curl -H \"hibp-api-key:b5e16e09e2fe4360830a08907d62353d\" -H \"user-agent: Beyond the Frame\" -sS https://haveibeenpwned.com/api/v3/breachedaccount/\{fname}\?truncateResponse=false".format(fname = input)
+            hbpwd = subprocess.run(arg_hbpd, stdout=subprocess.PIPE ,text=True ,shell=True)
+            hbpwd = jsbeautifier.beautify(hbpwd.stdout)
+
+# dehashed
             arg2 = "curl 'https://api.dehashed.com/search?query={}' -u zeifoa5@gmail.com:i0xtde61tvcrmm4skwvx8atdrbv1zmed -H 'Accept: application/json'".format(input)
             dehashed = subprocess.run(arg2, capture_output=True, shell=True, encoding="utf8")
             res = jsbeautifier.beautify(dehashed.stdout)
-            res = res+'\n'+"HAVEIBEENPWND"+'\n'+hbpwd.stdout
+# combining             
+            res = res+ '\n'+'\n'+hbpwd
+
             res = res.replace('\n', '<br>')
         except :
-            res = 'Invalid Input'            
+            res = 'Invalid Input'   
+
+#genarating report
+        session['res'] = res
+        write()      
+                  
         return render_template("result.html" , out = res)
+        
     else:
         return render_template("email.html")
 # ____________________________________________________________________________________________________________________________________________
@@ -110,6 +139,7 @@ def Facebook():
     if request.method == "POST":
         
         try :
+# Elastic    
             mob = request.form["mobile"]
             number = mob
             arg  = "curl -X GET 'http://localhost:9200/eg/_search?q=\"{fname}\"'".format(fname = number) 
@@ -123,15 +153,36 @@ def Facebook():
                 fb_id = data['hits']['hits'][0]['_source']['id']  
                 phone = data['hits']['hits'][0]['_source']['phone']  
                 res = "Name: " +str(name)+  "\nID : " +fb_id+ "\nphone number : "+phone
-                res = res.replace('\n', '<br>')
+                res_elk = res.replace('\n', '<br>')
+# dehashed
+                arg_dehashed = "curl 'https://api.dehashed.com/search?query={}' -u zeifoa5@gmail.com:i0xtde61tvcrmm4skwvx8atdrbv1zmed -H 'Accept: application/json'".format(phone)
+                dehashed = subprocess.run(arg_dehashed, capture_output=True, shell=True, encoding="utf8")
+                res_dehashed = jsbeautifier.beautify(dehashed.stdout)                                
 
             else : 
                 res = number +" doesn't seem to be in Facebook breach"
 
+#have ibeenpwnd
+
+            arg_hbpd = "curl -H \"hibp-api-key:b5e16e09e2fe4360830a08907d62353d\" -H \"user-agent: Beyond the Frame\" -sS https://haveibeenpwned.com/api/v3/breachedaccount/\{fname}\?truncateResponse=false".format(fname = phone)
+            hbpwd = subprocess.run(arg_hbpd, stdout=subprocess.PIPE ,text=True ,shell=True)
+            hbpwd = jsbeautifier.beautify(hbpwd.stdout)
+#combin
+            # res = res_elk+ '\n'+'\n'+hbpwd+ '\n'+'\n'+res_dehashed
+            res = res_elk+ '\n'+'\n'+hbpwd
+            res = res.replace('\n', '<br>')
+            
+
         except  : 
             res = "Invalid Input , please enter FB id or a mobile number beginning with the country id"
+     
+#genarating report
+        session['res'] = res
+        write()
+                     
 
         return render_template("result.html", out = res)
+        
     else:
         return render_template("Facebook.html")
 
@@ -152,16 +203,21 @@ def org():
             # res = jsbeautifier.beautify(dehashed.stdout)
 #            res = pprint.pformat(dehashed.stdout , indent = 3)
             
-            res = filt.stdout
-            res = res.replace('\n', '<br>')
+            res = filt.stdout.replace('\n', '<br>')
+            # res = res.replace('\n', '<br>')
 
         except : 
             res = 'invalid input'
-        return render_template("result.html" , out = res)
+
+#genarating report
+        session['res'] = res
+        write()
+
+        return render_template("result.html" , out = res) 
+        
+
     else:
         return render_template("org.html")
-
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=2005 , host='0.0.0.0')
