a8658d99a77419b3c129f81cfd4dafcc0880ca57
Etwas <73107895+Seif-AW@users.noreply.github.com>
2021-07-01 17:31:56 +0200

    Delete graduation directory

--- graduation/checkpwnedemails/csv/convert.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-with open('dehashed.json', encoding='utf-8-sig') as f_input:
-    df = pd.read_json(f_input)
-
-df.to_csv('dehashed.csv', encoding='utf-8', index=False)
--- graduation/checkpwnedemails/filterjson.py
+++
@@ -1,7 +0,0 @@
-import json
-import pprint
-
-data = json.load(open('jsontest.txt'))
-p = pprint.pformat(data , indent = 3)
-
-print(p)
